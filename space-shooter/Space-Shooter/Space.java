6f5d7666e34c3aa3cb745fb76f3b7a73a693226a
Markus Geilehner <markus@geilehner.io>
2020-04-02 10:50:30 +0200

    Basic Movement

--- space-shooter/Space-Shooter/Space.java
+++ space-shooter/Space-Shooter/Space.java
@@ -17,5 +17,16 @@ public class Space extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        SpaceWombat spaceWombat = new SpaceWombat();
+        addObject(spaceWombat,602,646);
     }
 }
--- space-shooter/Space-Shooter/SpaceWombat.java
+++ space-shooter/Space-Shooter/SpaceWombat.java
@@ -8,12 +8,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class SpaceWombat extends Actor
 {
+    private final int movementSpeed = 4;
     /**
      * Act - do whatever the SpaceWombat wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+        basicMovement();
+    }
+    
+    public void basicMovement(){
+        setRotation(-90);
+        if(Greenfoot.isKeyDown("left")){
+            setLocation(getX()-this.movementSpeed,getY());
+        }
+        else if(Greenfoot.isKeyDown("right")){
+            setLocation(getX()+this.movementSpeed,getY());
+        }
+    }
 }
